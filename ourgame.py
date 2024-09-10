aa84e3ce6a8177e9a9ea47c4ec3e3bc59859aebe
jbt <noya.weinberg6340@gmail.com>
2024-09-10 14:17:43 +0300

    first structure

--- ourgame.py
+++ ourgame.py
@@ -8,6 +8,14 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("benitoroei")
 
+running = True
+
+while running:
+    for event in pygame.event.get():
+
+        # Check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
 
 
 
