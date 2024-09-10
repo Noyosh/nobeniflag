8bf978c38833d98b33e1229bbb238daefbc9274f
jbt <noya.weinberg6340@gmail.com>
2024-09-10 14:43:39 +0300

    first structure

--- ourgame.py
+++ ourgame.py
@@ -1,12 +1,13 @@
 import pygame
 import pygame
-pygame.init()
 
 pygame.init()
 
-pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("benitoroei")
+
+x = 400
+y =400
 
 running = True
 
@@ -17,96 +18,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        keys=pygame.key.get_pressed()
 
+        if keys[pygame.K_LEFT]:
 
+        if keys[pygame.K_RIGHT]:
 
+        if keys[pygame.K_DOWN]:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if keys[pygame.K_UP]:
 
